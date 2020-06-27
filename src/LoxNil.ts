650f429c44ba15b7d10bde796ce6627926f2cced
David Timms <davidtimms@btinternet.com>
2020-06-28 00:40:01 +0100

    Changed LoxNil to use NativeTypeMixin instead of extending LoxInstance

--- src/LoxNil.ts
+++ src/LoxNil.ts
@@ -1,20 +1,34 @@
-import LoxInstance from "./LoxInstance";
-import * as globals from "./globals";
+import NativeTypeMixin from "./NativeTypeMixin";
+import { applyMixin } from "./helpers";
+import LoxClass from "./LoxClass";
 
 // store instance here to ensure LoxNil is a singleton
 let _nil: LoxNil | null = null;
 
-export class LoxNil extends LoxInstance {
+class LoxNil {
     readonly type = "NIL";
 
-    constructor(loxClass = globals.Nil) {
-        super(loxClass);
+    constructor() {
         if (_nil) return _nil;
+    }
+
+    get loxClass(): LoxClass {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        const globals = require("./globals");
+        Object.defineProperty(LoxNil.prototype, "loxClass", {
+            value: globals.Nil,
+        });
+        return globals.Nil;
     }
 
     toString(): string {
         return "nil";
     }
 }
+
+interface LoxNil extends NativeTypeMixin {}
+applyMixin(LoxNil, NativeTypeMixin);
+export { LoxNil };
+
 
 export const nil = _nil = new LoxNil();
